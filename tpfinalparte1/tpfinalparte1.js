75472a4b9707cbc7642362fee4934c132d84c8e8
micaelacardozo <micaelacar040@gmail.com>
2025-11-14 15:35:40 -0300

    Add files via upload

---
+++ tpfinalparte1/tpfinalparte1.js
@@ -0,0 +1,36 @@
+//Micaela Cardozo y Karen Quevedo
+//comisión 1
+//https://youtu.be/3pfjHtVNo8o 
+let imagenes = [];
+let frases = [];
+let estado = "inicio";
+let fondo, libro;
+let tiempo = 0;     
+
+let posXBoton, posYBoton, posXBoton2, posYBoton2, tamX, tamY;
+
+function preload() {
+  libro = loadSound('data/libro.mp3');
+   fondo = loadSound('data/historia.mp3');
+  for (let i = 0; i <=17; i++) {
+    imagenes[i] = loadImage("data/imagen" + i + ".jpg");
+  }
+}
+     
+function setup() {
+  createCanvas(640, 480);
+  fondo.amp(0.3);   
+  fondo.loop();
+  
+  
+  textSize(20);
+  narrativa();
+  botonesdedesicion();
+  
+   
+}
+
+function draw() {
+  background(0);
+ ordendehistoria ();
+}
